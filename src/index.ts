81c26c98d2405167b7bc54d0b834fa3cc2d1a578
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-12-29 19:41:19 +0100

    m

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-async function getCameraStream(): Promise<MediaStreamTrack> {
-  const userMedia = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  return userMedia.getVideoTracks()[0]
-}
+import * as tf from '@tensorflow/tfjs'
+import * as tmImage from '@teachablemachine/image'
 
-getCameraStream().then(mediaStream => {
-  console.log(mediaStream)
-}).catch(err => {
-  console.log(err)
-})
+window.onload = async () => {
+
+  const constrains: MediaStreamConstraints = {
+    audio: false,
+    video: {
+      width: 1920,
+      height: 1080,
+      frameRate: 60,
+    }
+  }
+
+  const userMedia = await navigator.mediaDevices.getUserMedia(constrains)
+
+  const video = document.getElementById('video') as HTMLVideoElement
+  video.width = userMedia.getVideoTracks()[0].getSettings().width
+  video.height = userMedia.getVideoTracks()[0].getSettings().height
+  video.srcObject = userMedia
+
+  const canvas = document.createElement('canvas')
+  canvas.width = video.width
+  canvas.height = video.height
+
+  const ctx = canvas.getContext('2d')
+
+  const model = await tmImage.loadFromFiles()
+  
+  
+  
+  const loop = setInterval(() => {
+    
+  }, 500)
+
+
+}
